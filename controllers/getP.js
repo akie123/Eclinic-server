109f9b3bd0636a3ce51db49644e065dfc6f6f7c7
bargavkoduri <bhargavkoduri123@gmail.com>
2023-04-28 03:25:32 +0530

    debug

--- controllers/getP.js
+++ controllers/getP.js
@@ -1,200 +1,205 @@
-const Patient = require("../models/patient")
-const Doctor  = require("../models/doctor")
-const Prescription = require("../models/prescription")
+const Patient = require("../models/patient");
+const Doctor = require("../models/doctor");
+const Prescription = require("../models/prescription");
 const Appointment = require("../models/appointment");
-const redisClient = require("../utils/redis")
+const redisClient = require("../utils/redis");
 
 const helperfun = (str) => {
-    let currentTime = new Date();
+  let currentTime = new Date();
 
-    let currentOffset = currentTime.getTimezoneOffset();
+  let currentOffset = currentTime.getTimezoneOffset();
 
-    let ISTOffset = 330; // IST offset UTC +5:30
+  let ISTOffset = 330; // IST offset UTC +5:30
 
-    let ISTTime = new Date(
-        currentTime.getTime() + (ISTOffset + currentOffset) * 60000
-    );
+  let ISTTime = new Date(
+    currentTime.getTime() + (ISTOffset + currentOffset) * 60000
+  );
 
-    // ISTTime now represents the time in IST coordinates
+  // ISTTime now represents the time in IST coordinates
 
-    let hoursIST = ISTTime.getHours();
-    let minutesIST = ISTTime.getMinutes();
-    let hours = str.substr(0, 2);
-    let minutes = str.substr(3, 2);
-    if (hours > hoursIST) return true;
-    else if (hours == hoursIST) return minutes > minutesIST;
-    else return false;
+  let hoursIST = ISTTime.getHours();
+  let minutesIST = ISTTime.getMinutes();
+  let hours = str.substr(0, 2);
+  let minutes = str.substr(3, 2);
+  console.log(hoursIST,"  ",minutesIST)
+  console.log(hours,"  ",minutes)
+  if (hours > hoursIST) return true;
+  else if (hours == hoursIST) return minutes > minutesIST;
+  else return false;
 };
 
 const checkPast = (obj) => {
-    const todaydate = new Date();
-    const date = new Date(obj.date);
-    if (todaydate.getFullYear() > date.getFullYear()) return true;
+  const todaydate = new Date();
+  const date = new Date(obj.date);
+  console.log(todaydate)
+  console.log(date)
+  if (todaydate.getFullYear() > date.getFullYear()) return true;
+  else {
+    if (todaydate.getMonth() > date.getMonth()) return true;
     else {
-        if (todaydate.getMonth() > date.getMonth()) return true;
-        else {
-            if (todaydate.getDate() > date.getDate()) return true;
-            else if(todaydate.getDate() < date.getDate()) return false;
-            else return !helperfun(obj.time);
-        }
+      if (todaydate.getDate() > date.getDate()) return true;
+      else if (todaydate.getDate() < date.getDate()) return false;
+      else return !helperfun(obj.time);
     }
+  }
 };
 
 const checkSlot = (str) => {
-    let currentTime = new Date();
+  let currentTime = new Date();
 
-    let currentOffset = currentTime.getTimezoneOffset();
+  let currentOffset = currentTime.getTimezoneOffset();
 
-    let ISTOffset = 330; // IST offset UTC +5:30
+  let ISTOffset = 330; // IST offset UTC +5:30
 
-    let ISTTime = new Date(
-        currentTime.getTime() + (ISTOffset + currentOffset) * 60000
-    );
+  let ISTTime = new Date(
+    currentTime.getTime() + (ISTOffset + currentOffset) * 60000
+  );
 
-    // ISTTime now represents the time in IST coordinates
+  // ISTTime now represents the time in IST coordinates
 
-    let hoursIST = ISTTime.getHours();
-    let minutesIST = ISTTime.getMinutes();
-    let hours = str.substring(0, 2);
-    let minutes = str.substring(3, 5);
-    let num = hours - hoursIST;
-    num *= 60;
-    let num1 = minutes - minutesIST;
-    return (num + num1) * 60 > 2000
-}
+  let hoursIST = ISTTime.getHours();
+  let minutesIST = ISTTime.getMinutes();
+  let hours = str.substring(0, 2);
+  let minutes = str.substring(3, 5);
+  let num = hours - hoursIST;
+  num *= 60;
+  let num1 = minutes - minutesIST;
+  return (num + num1) * 60 > 2000;
+};
 
-const getDoctors = async (req,res) => {
-    const {department} = req.query;
-    let cacheResults = await redisClient.get(department);
-    if(cacheResults){
-        cacheResults = JSON.parse(cacheResults)
-        for (let i = 0; i < cacheResults.length; i++) {
-            for (let j = 0; j < cacheResults[0].appointment.length; j++) {
-                if (
-                    cacheResults[i].appointment[j].avb === true &&
-                    !checkSlot(cacheResults[i].appointment[j].time)
-                ) {
-                    cacheResults[i].appointment[j].avb = false;
-                }
-            }
+const getDoctors = async (req, res) => {
+  const { department } = req.query;
+  let cacheResults = await redisClient.get(department);
+  if (cacheResults) {
+    cacheResults = JSON.parse(cacheResults);
+    for (let i = 0; i < cacheResults.length; i++) {
+      for (let j = 0; j < cacheResults[0].appointment.length; j++) {
+        if (
+          cacheResults[i].appointment[j].avb === true &&
+          !checkSlot(cacheResults[i].appointment[j].time)
+        ) {
+          cacheResults[i].appointment[j].avb = false;
         }
-        res.send(cacheResults)
+      }
     }
-    else {
-        Doctor.find(
-            { verified: true, spec: department },
-            {
-                name: 1,
-                spec: 1,
-                fees: 1,
-                exp: 1,
-                qualification: 1,
-                appointment: 1,
-                profilePic: 1,
-            }
-        ).then((resp) => {
-            for (let i = 0; i < resp.length; i++) {
-                for (let j = 0; j < resp[0].appointment.length; j++) {
-                    if (
-                        resp[i].appointment[j].avb === true &&
-                        !checkSlot(resp[i].appointment[j].time)
-                    ) {
-                        resp[i].appointment[j].avb = false;
-                    }
-                }
-            }
-            redisClient.set(department, JSON.stringify(resp));
-            res.send(resp);
-        });
-    }
-}
+    res.send(cacheResults);
+  } else {
+    Doctor.find(
+      { verified: true, spec: department },
+      {
+        name: 1,
+        spec: 1,
+        fees: 1,
+        exp: 1,
+        qualification: 1,
+        appointment: 1,
+        profilePic: 1,
+      }
+    ).then((resp) => {
+      for (let i = 0; i < resp.length; i++) {
+        for (let j = 0; j < resp[0].appointment.length; j++) {
+          if (
+            resp[i].appointment[j].avb === true &&
+            !checkSlot(resp[i].appointment[j].time)
+          ) {
+            resp[i].appointment[j].avb = false;
+          }
+        }
+      }
+      redisClient.set(department, JSON.stringify(resp));
+      res.send(resp);
+    });
+  }
+};
 
 const getUpcoming = async (req, res) => {
-    const { id } = req.params;
-    let name = await Patient.findById(id,{name:1})
-    if (name)
-        name = name.name;
-    Appointment.find({ idP: id }).then((resp) => {
-        let arr = [],arr1 = [];
-        resp.forEach((appointment) => {
-            if (!checkPast(appointment)){
-                arr.push(appointment);
-            }
-        });
-        if(arr.length > 0){
-            arr.forEach((appointment, indx) => {
-                Doctor.findById(appointment.idD, { name: 1, spec: 1,qualification: 1,exp: 1,profilePic: 1}).then(
-                    (doc) => {
-                        arr1.push({
-                            id: appointment._id,
-                            idD: appointment.idD,
-                            idP: id,
-                            time: appointment.time,
-                            name: doc.name,
-                            spec: doc.spec,
-                            profilePic: doc.profilePic,
-                            exp: doc.exp,
-                            qualification: doc.qualification,
-
-                        });
-                        if (arr1.length === arr.length)
-                            res.send({
-                                upcoming: arr1,
-                                name: name,
-                            });
-                    }
-                );
-            });
-        }
-        else{
-            res.send({
-                upcoming: [],
-                name: name
-            })
-        }
+  const { id } = req.params;
+  let name = await Patient.findById(id, { name: 1 });
+  if (name) name = name.name;
+  Appointment.find({ idP: id }).then((resp) => {
+    let arr = [],
+      arr1 = [];
+    resp.forEach((appointment) => {
+      if (!checkPast(appointment)) {
+        arr.push(appointment);
+      }
     });
+    if (arr.length > 0) {
+      arr.forEach((appointment, indx) => {
+        Doctor.findById(appointment.idD, {
+          name: 1,
+          spec: 1,
+          qualification: 1,
+          exp: 1,
+          profilePic: 1,
+        }).then((doc) => {
+          arr1.push({
+            id: appointment._id,
+            idD: appointment.idD,
+            idP: id,
+            time: appointment.time,
+            name: doc.name,
+            spec: doc.spec,
+            profilePic: doc.profilePic,
+            exp: doc.exp,
+            qualification: doc.qualification,
+          });
+          if (arr1.length === arr.length)
+            res.send({
+              upcoming: arr1,
+              name: name,
+            });
+        });
+      });
+    } else {
+      res.send({
+        upcoming: [],
+        name: name,
+      });
+    }
+  });
 };
 
 const getPast = async (req, res) => {
-    const { id } = req.params;
-    Appointment.find({ idP: id }).then((resp) => {
-        let arr = [],
-            arr1 = [];
-        resp.forEach((appointment) => {
-            if (checkPast(appointment)) arr.push(appointment);
-        });
-        if (arr.length > 0) {
-            arr.forEach((appointment, indx) => {
-                Doctor.findById(appointment.idD, { name: 1, spec: 1 }).then((pat) => {
-                    arr1.push({
-                        id: appointment._id,
-                        idP: appointment.idP,
-                        idD: id,
-                        date: appointment.date,
-                        spec: pat.spec,
-                        time: appointment.time,
-                        name: pat.name,
-                    });
-                    if (arr1.length === arr.length){
-                        for(let i = 0;i < arr1.length;i++){
-                            Prescription.findOne({app_id : arr1[i].id}).then(resp => {
-                                arr1[i].prescription = resp
-                                if(i == arr1.length-1){
-                                    res.send({
-                                        past : arr1
-                                    })
-                                }
-                            })
-                        }
-                    }
-                });
-            });
-        } else {
-            res.send({
-                past: [],
-            });
-        }
+  const { id } = req.params;
+  Appointment.find({ idP: id }).then((resp) => {
+    let arr = [],
+      arr1 = [];
+    resp.forEach((appointment) => {
+      if (checkPast(appointment)) arr.push(appointment);
     });
+    if (arr.length > 0) {
+      arr.forEach((appointment, indx) => {
+        Doctor.findById(appointment.idD, { name: 1, spec: 1 }).then((pat) => {
+          arr1.push({
+            id: appointment._id,
+            idP: appointment.idP,
+            idD: id,
+            date: appointment.date,
+            spec: pat.spec,
+            time: appointment.time,
+            name: pat.name,
+          });
+          if (arr1.length === arr.length) {
+            for (let i = 0; i < arr1.length; i++) {
+              Prescription.findOne({ app_id: arr1[i].id }).then((resp) => {
+                arr1[i].prescription = resp;
+                if (i == arr1.length - 1) {
+                  res.send({
+                    past: arr1,
+                  });
+                }
+              });
+            }
+          }
+        });
+      });
+    } else {
+      res.send({
+        past: [],
+      });
+    }
+  });
 };
 
-module.exports = {getDoctors,getUpcoming,getPast}
+module.exports = { getDoctors, getUpcoming, getPast };
--- server.js
+++ server.js
@@ -1,152 +1,151 @@
-const express = require("express")
-const mongoose = require("mongoose")
-const app = express()
-const cors = require("cors")
-const morgan = require("morgan")
-const {accessLogStream} = require("./utils/morgan")
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const cors = require("cors");
+const morgan = require("morgan");
+const { accessLogStream } = require("./utils/morgan");
 const swaggerUI = require("swagger-ui-express");
-const swaggerJsDoc = require("swagger-jsdoc")
+const swaggerJsDoc = require("swagger-jsdoc");
 
 // Routers
-const PatientRouter = require("./routes/patient")
-const DoctorRouter = require("./routes/doctor")
-const AdminRouter = require("./routes/admin")
-const LoginRouter = require("./routes/login")
-const RegisterRouter = require("./routes/register")
-const ContactusRouter = require("./routes/contactus")
-const CancelRouter = require("./routes/cancel")
-const VerifyRouter =require("./routes/verify")
-const PrescriptionRouter = require("./routes/prescription")
+const PatientRouter = require("./routes/patient");
+const DoctorRouter = require("./routes/doctor");
+const AdminRouter = require("./routes/admin");
+const LoginRouter = require("./routes/login");
+const RegisterRouter = require("./routes/register");
+const ContactusRouter = require("./routes/contactus");
+const CancelRouter = require("./routes/cancel");
+const VerifyRouter = require("./routes/verify");
+const PrescriptionRouter = require("./routes/prescription");
 // constanst
-const { MONGO_URI,PORT } = require('./constants')
+const { MONGO_URI, PORT } = require("./constants");
 
 // Middlewares
-app.use(cors())      // cors middleware
+app.use(cors({ origin: true })); // cors middleware
 app.use(morgan("tiny", { stream: accessLogStream }));
 app.use(express.json({ limit: "2mb" }), (err, req, res, next) => {
   // bodyparse middle ware checks for valid body format
-  if (err) 
-    res.sendStatus(400);
-  else 
-    next();
+  if (err) res.sendStatus(400);
+  else next();
 });
 
 // Connection to Database
 
 mongoose.set("strictQuery", true);
-mongoose.connect(MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => {
-    app.listen(PORT,() => {
-        console.log(`Server Running on PORT ${PORT}`)
-    })
-})
+mongoose
+  .connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server Running on PORT ${PORT}`);
+    });
+  });
 
+const cron = require("node-cron");
+const Doctor = require("./models/doctor");
 
+cron.schedule("0 0 * * *", async () => {
+  let docs = await Doctor.find({});
 
+  for (let i = 0; i < docs.length; i++) {
+    for (let j = 0; j < docs[i].appointment.length; j++) {
+      const appointmentDate = docs[i].appointment[j].date;
+      const currentDate = new Date().toDateString();
 
-const cron = require('node-cron');
-const Doctor = require('./models/doctor');
-
-cron.schedule('0 0 * * *', async () => {
-    let docs = await Doctor.find({});
-
-    for (let i = 0; i < docs.length; i++) {
-        for (let j = 0; j < docs[i].appointment.length; j++) {
-            const appointmentDate = docs[i].appointment[j].date;
-            const currentDate = new Date().toDateString();
-
-            if (docs[i].appointment[j].patientId!="" && appointmentDate != currentDate) {
-                docs[i].appointment[j].avb = true;
-                docs[i].appointment[j].patientId="";
-            }
-        }
-
-        await docs[i].save();
+      if (
+        docs[i].appointment[j].patientId != "" &&
+        appointmentDate != currentDate
+      ) {
+        docs[i].appointment[j].avb = true;
+        docs[i].appointment[j].patientId = "";
+      }
     }
+
+    await docs[i].save();
+  }
 });
 const options = {
-    definition: {
-        openapi: "3.0.0",
-        info: {
-            title: "E-Clinic",
-            version: "1.0.0",
-            description: "A simple Express Library API",
-        },
-        components: {
-            securitySchemes: {
-                bearerAuth: {
-                    type: 'http',
-                    scheme: 'bearer',
-                    bearerFormat: 'JWT',
-                }
-            }
-        },
-        security: [{
-            bearerAuth: []
-        }],
-        servers: [
-            {
-                url: "http://localhost:5000",
-            },
-        ],
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "E-Clinic",
+      version: "1.0.0",
+      description: "A simple Express Library API",
     },
-    apis: ["./routes/*.js"],
-
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: "http",
+          scheme: "bearer",
+          bearerFormat: "JWT",
+        },
+      },
+    },
+    security: [
+      {
+        bearerAuth: [],
+      },
+    ],
+    servers: [
+      {
+        url: "http://localhost:5000",
+      },
+    ],
+  },
+  apis: ["./routes/*.js"],
 };
 
 const specs = swaggerJsDoc(options);
-const Patient = require("./models/patient")
-const Doctors = require("./models/doctor")
-app.post("/addIdP",(req,res)=>{
-    console.log(req.body)
-    Patient.findByIdAndUpdate(req.body.id, { socketId: req.body.sid })
-        .then(() => {
-            res.json({ status: 'success' });
-        })
-        .catch((err) => {
-            res.json({ status: 'failed' });
-        });
-})
-app.post("/addIdD",(req,res)=>{
-    console.log(req.body)
-    Doctor.findByIdAndUpdate(req.body.id, { socketId: req.body.sid })
-        .then(() => {
-            res.json({ status: 'success' });
-        })
-        .catch((err) => {
-            res.json({ status: 'failed' });
-        });
-})
-
-app.get('/pat/:idP',async(req,res)=>{
-    Patient.findById(req.params.idP).then(pat => {
-        if(pat)
-        res.status(201).json({ socket:pat.socketId })
-        else
-            res.status(201).json({ socket:"" })
-    }).catch(err => {
-        res.status(201).json({ socket:"" });
+const Patient = require("./models/patient");
+const Doctors = require("./models/doctor");
+app.post("/addIdP", (req, res) => {
+  console.log(req.body);
+  Patient.findByIdAndUpdate(req.body.id, { socketId: req.body.sid })
+    .then(() => {
+      res.json({ status: "success" });
+    })
+    .catch((err) => {
+      res.json({ status: "failed" });
     });
+});
+app.post("/addIdD", (req, res) => {
+  console.log(req.body);
+  Doctor.findByIdAndUpdate(req.body.id, { socketId: req.body.sid })
+    .then(() => {
+      res.json({ status: "success" });
+    })
+    .catch((err) => {
+      res.json({ status: "failed" });
+    });
+});
 
-})
-
+app.get("/pat/:idP", async (req, res) => {
+  Patient.findById(req.params.idP)
+    .then((pat) => {
+      if (pat) res.status(201).json({ socket: pat.socketId });
+      else res.status(201).json({ socket: "" });
+    })
+    .catch((err) => {
+      res.status(201).json({ socket: "" });
+    });
+});
 
 // Routes
 app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
 // Routes
-app.use('/verify', VerifyRouter);
-app.use("/login",LoginRouter)           // Login Router
-app.use("/register",RegisterRouter)     // Register Router
-app.use("/cancel",CancelRouter)
-app.use("/contact-us",ContactusRouter)  // Contact us Router
-app.use("/patient",PatientRouter)       // Patient Router
-app.use("/doctor",DoctorRouter)         // Doctor Router
-app.use("/admin",AdminRouter)           // Admin Router
-app.use("/prescription",PrescriptionRouter) // Prescription Router
-app.use("*",(req,res) => {
-  res.sendStatus(404)                   // No path found
-})
+app.use("/verify", VerifyRouter);
+app.use("/login", LoginRouter); // Login Router
+app.use("/register", RegisterRouter); // Register Router
+app.use("/cancel", CancelRouter);
+app.use("/contact-us", ContactusRouter); // Contact us Router
+app.use("/patient", PatientRouter); // Patient Router
+app.use("/doctor", DoctorRouter); // Doctor Router
+app.use("/admin", AdminRouter); // Admin Router
+app.use("/prescription", PrescriptionRouter); // Prescription Router
+app.use("*", (req, res) => {
+  res.sendStatus(404); // No path found
+});
 
-module.exports = app
+module.exports = app;
